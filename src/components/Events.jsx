ede2fd8ae0de1bdf469547ecc807f24aaa6bcff5
Chih-Lin Lee <mlee749@gmail.com>
2019-03-12 14:49:51 -0700

    events rendered on timeline

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../index.css';
-
+import functions from '../functions.js';
 import timelineItems from '../timelineItems.js';
 
 class Events extends Component {
@@ -8,12 +8,40 @@ class Events extends Component {
     super(props);
   }
 
+  calculateTopPosition(obj) {
+    return functions.calculateTopPosition(obj);
+  };
+
+  calculateWidth(obj) {
+    return functions.distanceWidth * functions.calculateEventWidth(obj);
+  };
+
+  calculateStart(obj) {
+    return functions.distanceWidth * functions.calculateLeftPosition(obj);
+  };
+
+  changeBackground(obj) {
+    return functions.giveEventColor(obj.id);
+  };
+
   render() {
     return (
       <div>
         {timelineItems.map(event => (
-          <div key={event.id} className='eventBox'>
-            {event.name}
+          <div 
+            key={event.id} 
+            className='eventBox'
+            draggable 
+            style= {{
+              top: this.calculateTopPosition(event),
+              width: this.calculateWidth(event),
+              left: this.calculateStart(event),
+              background: this.changeBackground(event),
+              }}>
+            <span 
+              id={event.id}>
+              {event.name}
+            </span>
           </div>
         ))}
       </div>
